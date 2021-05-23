6d5f24fdaf401ad6a5584a56971277d03485aacd
Amine-tek <94574896+aminos-tech@users.noreply.github.com>
2021-05-23 16:21:37 +0200

    first commit

---
+++ src/test/java/de/tekup/test/invoke/InvokeSoapTestWsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tekup.test.invoke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvokeSoapTestWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
